fe0cad8c12d8b0c99f04d42a6bc395212a2046a1
RansomBroker <yadisoke@gmail.com>
2023-10-19 20:53:35 +0700

    menambahkan datatables boostrap4

--- resources/views/includes/css.blade.php
+++ resources/views/includes/css.blade.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/sb-admin-2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/dataTables.bootstrap4.min.css') }}">
 
--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -2,3 +2,5 @@
 <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
 <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/js/dataTables.boostrap4.min.js') }}"></script>
