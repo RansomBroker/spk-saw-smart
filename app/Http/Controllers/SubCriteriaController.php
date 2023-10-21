0d033816e64d4936f464df571b704dd7c9632ac4
RansomBroker <yadisoke@gmail.com>
2023-10-21 12:36:12 +0700

    membuat method di controller

--- app/Http/Controllers/SubCriteriaController.php
+++ app/Http/Controllers/SubCriteriaController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SubCriteria;
 use Illuminate\Http\Request;
 
 class SubCriteriaController extends Controller
 {
-    //
+    public function subCriteriaView()
+    {
+
+    }
+
+    public function subCriteriaAdd(SubCriteria $subCriteria, Request $request)
+    {
+
+    }
+
+    public function subCriteriaEdit(SubCriteria $subCriteria, Request $request)
+    {
+
+    }
+
+    public function subCriteriaDelete(SubCriteria $subCriteria, Request $request)
+    {
+
+    }
 }
