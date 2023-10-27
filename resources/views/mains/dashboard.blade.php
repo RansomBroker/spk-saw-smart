f8fc8bddc74e1e16713901db24eebbabb2988c80
RansomBroker <yadisoke@gmail.com>
2023-10-27 10:34:45 +0700

    membatasi hak akses di dashboad

--- resources/views/mains/dashboard.blade.php
+++ resources/views/mains/dashboard.blade.php
@@ -23,76 +23,93 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <a href="{{ route('criteria.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Kriteria</a>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-fw fa-chart-area fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <a href="{{ route('subcrit.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Sub Kriteria</a>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fa fa-th-list fa-2x text-gray-300"></i>
+        @if(Auth::user()->role == 1)
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <a href="{{ route('criteria.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Kriteria</a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-fw fa-chart-area fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <a href="{{ route('candidate.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Calon</a>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fa fa-users fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <a href="{{ route('calculate.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Perhitungan</a>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fa fa-calculator fa-2x text-gray-300"></i>
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <a href="{{ route('subcrit.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Sub Kriteria</a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-th-list fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <a href="{{ route('calculate.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Data Hasil Akhir (perbandingan)</a>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-fw fa-chart-area fa-2x text-gray-300"></i>
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <a href="{{ route('candidate.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Calon</a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-users fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <a href="{{ route('calculate.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Perhitungan</a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fa fa-calculator fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <a href="{{ route('compare.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Data Hasil Akhir (perbandingan)</a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-fw fa-chart-area fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @else
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <a href="{{ route('compare.view') }}" class="h5 mb-0 font-weight-bold text-gray-800">Data Hasil Akhir (perbandingan)</a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-fw fa-chart-area fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
 
 @endsection
